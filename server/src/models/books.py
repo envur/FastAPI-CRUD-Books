73b99082bf1826cf02acc11164b19726a0811d60
Vittoria Tossini <gabrielstefanello@hotmail.com>
2021-10-03 21:03:05 -0300

    Alteração no books

--- server/src/models/books.py
+++ server/src/models/books.py
@@ -9,7 +9,7 @@ class Books(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False)
+    title = Column(String(100), nullable=False, unique=True)
     author = Column(String(90), nullable=False)
     read = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
